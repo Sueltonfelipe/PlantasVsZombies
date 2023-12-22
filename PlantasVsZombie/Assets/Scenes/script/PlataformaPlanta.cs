d3f6485362fe90ee05cc211e94ec9d3086ed17eb
Suelton Felipe Alves Pereira <suelton.a@academico.ifrn.edu.br>
2023-12-22 09:57:55 -0300

    ???

--- PlantasVsZombie/Assets/Scenes/script/PlataformaPlanta.cs
+++ PlantasVsZombie/Assets/Scenes/script/PlataformaPlanta.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 
 public class PlataformaPlanta : MonoBehaviour
 {
-    public GameObject objetoACriarBotaoEsquerdo;  // Objeto a ser criado ao clicar com o botão esquerdo
-    public GameObject objetoACriarBotaoDireito;   // Objeto a ser criado ao clicar com o botão direito
-    public GameObject objetoDeReferencia;         // Objeto de referência para verificar o clique
-
+    public GameObject objetoACriarBotaoEsquerdo; 
+    public GameObject objetoACriarBotaoDireito;  
+    public GameObject objetoDeReferencia;
+    public int Moeda;
     void Update()
     {
-        // Verifica se o botão esquerdo do mouse foi clicado
+       
         if (Input.GetMouseButtonDown(0))
         {
-            // Cria o objeto correspondente ao botão esquerdo apenas se o clique for no objeto de referência
+           
             if (CliqueNoObjeto(objetoDeReferencia))
             {
                 Instantiate(objetoACriarBotaoEsquerdo, transform.position, Quaternion.identity);
             }
         }
 
-        // Verifica se o botão direito do mouse foi clicado
+       
         if (Input.GetMouseButtonDown(1))
         {
-            // Cria o objeto correspondente ao botão direito apenas se o clique for no objeto de referência
+           
             if (CliqueNoObjeto(objetoDeReferencia))
             {
                 Instantiate(objetoACriarBotaoDireito, transform.position, Quaternion.identity);
@@ -29,13 +29,13 @@ public class PlataformaPlanta : MonoBehaviour
         }
     }
 
-    // Função para verificar se o clique foi no objeto de referência
+
     bool CliqueNoObjeto(GameObject objeto)
     {
         Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        // Verifica se o raio atinge o objeto de referência
+      
         return Physics.Raycast(raio, out hit) && hit.collider.gameObject == objeto;
     }
 }
